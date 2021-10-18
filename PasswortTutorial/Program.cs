5b13ea8af2cd95b43373eaa46c9ba6c7850b6f3d
Fabio Gaming <fabio.furch@gmail.com>
2021-10-18 14:46:56 +0200

    Projektdateien hinzufügen.

---
+++ PasswortTutorial/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace PasswortTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Login();
+
+        }
+
+
+        static void Login()
+        {
+            Console.Title = "C# Login";
+            extras.PrintColor("Bitte gebe das Passwort an", "blue");
+            extras.PrintColor("oder Verlasse mit 'exit'", "blue");
+
+            string input = Console.ReadLine();
+
+            if(input == "9978")
+            {
+
+                Geschafft();
+            } else
+            {
+                if(input == "exit")
+                {
+                    Environment.Exit(0);
+
+                } else
+                {
+
+                    extras.PrintColor($@"{input} ist nicht das richtige Passwort", "red");
+                    extras.PrintColor("bitte gebe das richtige Passwort an", "red");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                    Login();
+
+
+
+                }
+
+            }
+
+        }
+
+        static void Geschafft()
+        {
+
+            extras.PrintColor("Du hast es Geschafft", "green");
+            Console.ReadKey();
+
+
+        } 
+
+
+
+
+    }
+}
---
+++ PasswortTutorial/extras.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswortTutorial
+{
+    class extras
+    {
+
+
+        public static void PrintColor(string message, String color)
+        {
+            switch (color)
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case "dark_green":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "gray":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case "black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "dark_blue":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "dark_cyan":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case "dark_gray":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "dark_magenta":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "dark_red:":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "dark_yellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "gray:":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case "magenta":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Color");
+                    break;
+            }
+            Console.WriteLine(message);
+            Console.ResetColor();
+
+        }
+    }
+}
